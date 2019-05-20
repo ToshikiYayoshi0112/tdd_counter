efc771c0dfdb0fb3e9644f3439078a72036cd988
Yayoshi <sagauyayoshi@gmail.com>
2019-05-20 20:08:07 +0900

    カウンタークラス初期値を設定できるように関数を作った。 テストコードを書くところからクラスを設計した。

---
+++ test_counter.py
@@ -0,0 +1,30 @@
+import unittest
+
+"""
+-[ ] 整数値をカウントアップできる
+    -[ ] 初期値は自由に設定できる
+        -[x] 初期値0に設定できる
+        -[x] 初期値を15に設定できる
+    -[ ] カウントアップは1ずつ
+"""
+
+
+class Counter:
+    def __init__(self, value):
+        self.value = value
+
+
+class TestCounter(unittest.TestCase):
+    def test_カウンターの初期値を0に設定できる(self):
+        my_counter = Counter(0)
+
+        self.assertEqual(0, my_counter.value)
+
+    def test_カウンターの初期値を15に設定できる(self):
+        my_counter = Counter(15)
+
+        self.assertEqual(15, my_counter.value)
+
+
+if __name__ == "__main__":
+    unittest.main()
